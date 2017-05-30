ad475fd8523bab66166954e40ac339d27c00a41a
Esdras Eduardo <esdrasedu@gmail.com>
2017-05-29 22:48:09 -0300

    make filter in ets

---
+++ lib/regex_case.ex
@@ -0,0 +1,17 @@
+defmodule RegexCase do
+  defmacro regex_case(string, do: lines) do
+    new_lines = Enum.map lines, fn ({:->, context, [[regex], result]}) ->
+      condition = quote do: String.match?(unquote(string), unquote(regex))
+      {:->, context, [[condition], result]}
+    end
+
+    base_case = quote do: (true -> nil)
+    new_lines = new_lines ++ base_case
+
+    quote do
+      cond do
+        unquote(new_lines)
+      end
+    end
+  end
+end
